edb63e49cf9d3cd8a7f71eb223690ccd4fe3a0ff
uairc <chenairc@gmail.com>
2025-07-28 13:15:56 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '1b244b7a-eb15-4e16-813f-cefaa2fdca90';
-let proxyIP = '';
+let proxyIP = 'proxyip.cmliussss.net:443';
 let DNS64Server = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
